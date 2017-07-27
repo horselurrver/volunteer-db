d30d0efcbaafa97467e010b9a2475d23411bcdab
Amy Wang <horselurrver@gmail.com>
2017-07-27 10:36:53 -0700

    initial commit

---
+++ models/models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI;
+var Schema = mongoose.Schema;
+
+mongoose.connect(connect);
+
+var studentSchema = new Schema({
+  fullName: String,
+  password: String,
+  organizations: {
+    type: Array,
+    default: []
+  },
+  hours: {
+    type: Number,
+    default: 0
+  }
+});
+
+var formSchema = new Schema({
+  studentName: String,
+  location: String,
+  hours: Number,
+  organization: String
+});
+
+var Student = mongoose.model('Student', studentSchema);
+var Form = mongoose.model('Form', formSchema);
+
+module.exports = {
+  Student: Student,
+  Form: Form
+};
---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const models = require('./models/models');
+const { Student, Form } = models;
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Successfully connected to mongoose!');
+});
+
+mongoose.connection.on('error', function () {
+  console.log('Mongoose connection error');
+});
+
+app.use(logger('dev'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+})
+
+
+//register, login, submit form
+app.post('/register', function(req, res) {
+  const name = req.body.fullName;
+  const password = req.body.password;
+  Student.findOne({fullName: name}, function(err, student) {
+    if (err) {
+      res.json({success: false, error: err});
+    } else if (!student) {
+      const stud = new Student({
+        fullName: name,
+        password: password
+      });
+      stud.save();
+      res.json({success: true});
+    } else {
+      res.json({success: false, error: 'Username taken'});
+    }
+  });
+});
+
+app.post('/login', function(req, res) {
+  const fullName = req.body.fullName;
+  const password = req.body.password;
+  Student.findOne({fullName: fullName, password: password}, function(err, student) {
+    if (err) {
+      res.json({success: false, error: err});
+    } else if (!student) {
+      res.json({success: false});
+    } else {
+      res.json({success: true});
+    }
+  });
+});
+
+app.post('/submitform', function(req, res) {
+  const studentName = req.body.studentName;
+  const location = req.body.location;
+  const hours = req.body.hours;
+  const organization = req.body.organization;
+  const newForm = new Form({
+    studentName: studentName,
+    location: location,
+    hours: hours,
+    organization: organization
+  });
+  newForm.save(function(err, form) {
+    if (err) {
+      res.json({success: false, error: err});
+    } else {
+      Student.findOne({fullName: studentName}, function(err, student) {
+        if (err) {
+          res.json({success: false, error: err});
+        } else {
+          student.hours += parseInt(hours);
+          student.organizations.push(organization);
+          student.save();
+          res.json({success: true});
+        }
+      });
+    }
+  });
+});
