d1007ffd7b35db5376b4f6e487f25e06197841e0
Amy Wang <horselurrver@gmail.com>
2017-07-27 10:49:18 -0700

    Fixed port

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 })
 
